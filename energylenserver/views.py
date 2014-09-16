a44ddb6b5b6c4a12cc2657772d37cbd7b1ec262e
Manaswi Saha <manaswis@iiitd.ac.in>
2014-09-16 16:13:15 +0530

    Rearranged views.py

--- energylenserver/views.py
+++ energylenserver/views.py
@@ -13,6 +13,7 @@ from meter.smap import *
 from functions import *
 from energylenserver.api.reassign import *
 from energylenserver.tasks import phoneDataHandler
+from wifi.functions import determine_user_home_status
 
 import os
 import sys
@@ -20,6 +21,10 @@ import json
 import time
 import pandas as pd
 import datetime as dt
+
+"""
+Helper functions
+"""
 
 
 def user_exists(device_id):
@@ -32,6 +37,185 @@ def user_exists(device_id):
     except RegisteredUsers.DoesNotExist, e:
         print "[UserDoesNotExistException Occurred] No registration found!:: ", e
         return False
+
+"""
+Registration API
+"""
+
+
+@csrf_exempt
+def register_device(request):
+    """
+    Receives the registration requests from the mobile devices and
+    stores user-device and meter details in the database
+    """
+    print "Request received:", request.method
+
+    try:
+        if request.method == 'GET':
+            return HttpResponse(json.dumps(ERROR_INVALID_REQUEST), content_type="application/json")
+
+        if request.method == 'POST':
+            print "\n[POST Request Received]"
+
+            # print request.body
+            payload = json.loads(request.body)
+            print "POST Payload:\n", payload
+
+            reg_id = payload['registration_id']
+            user_name = payload['user_name']
+            email_id = payload['email_id']
+            dev_id = payload['dev_id']
+            apt_no = payload['apt_no']
+            home_ap = payload['home_ap']
+            other_ap = payload['other_ap']
+
+            print "\n--User Registration Details--"
+            print "RegID:", reg_id
+            print "Username:", user_name
+            print "Email ID:", email_id
+            print "Device ID:", dev_id
+            print "Apartment Number:", apt_no
+            print "Home AP:", home_ap
+            print "Other APs:", other_ap
+
+            if apt_no.isdigit():
+
+                # Store the meter details of the apt_no if the current user is the first
+                # member of the house that registers
+
+                apt_no = int(apt_no)
+                user_count = RegisteredUsers.objects.filter(apt_no__exact=apt_no).count()
+                print "Number of users registered for", apt_no, ":", user_count
+                if user_count == 0:
+                    # Get meter information for the apt_no for the apartment
+                    meters = get_meter_info(apt_no)
+
+                    # Store meter information in the DB
+                    for meter in meters:
+                        meter_uuid = meter['uuid']
+                        meter_type = meter['type']
+                        if meter_type == "Light Backup":
+                            meter_type = "Light"
+
+                        minfo_record = MeterInfo(
+                            meter_uuid=meter_uuid, meter_type=meter_type, apt_no=apt_no)
+                        minfo_record.save()
+
+                print "Home AP:", home_ap
+
+                try:
+                    AccessPoints.objects.filter(apt_no__exact=apt_no).delete()
+
+                    # Store the access point details
+                    ap_record = AccessPoints(
+                        apt_no=apt_no, macid=home_ap['macid'], ssid=home_ap['ssid'], home_ap=True)
+                    ap_record.save()
+
+                    for ap in other_ap:
+                        ap_record = AccessPoints(
+                            apt_no=apt_no, macid=ap['macid'], ssid=ap['ssid'], home_ap=False)
+                        ap_record.save()
+                except Exception, e:
+                    print("[APSaveException]::%s" % (e))
+
+            try:
+                r = RegisteredUsers.objects.get(dev_id__exact=dev_id)
+                print "Registration with device ID", r.dev_id, "exists"
+                # Store user
+                r.reg_id = reg_id
+                r.name = user_name
+                r.is_active = True
+                if apt_no != 0:
+                    r.apt_no = apt_no
+                r.modified_date = dt.datetime.fromtimestamp(time.time())
+                r.save()
+                print "Registration updated"
+            except RegisteredUsers.DoesNotExist, e:
+
+                # Store user
+                user = RegisteredUsers(
+                    dev_id=dev_id, reg_id=reg_id, apt_no=apt_no, name=user_name, email_id=email_id)
+                user.save()
+                print "Registration successful"
+            return HttpResponse(json.dumps(REGISTRATION_SUCCESS),
+                                content_type="application/json")
+
+    except Exception, e:
+
+        print "[DeviceRegistrationException Occurred]::", e
+        print "Registration unsuccessful"
+        return HttpResponse(json.dumps(REGISTRATION_UNSUCCESSFUL), content_type="application/json")
+
+"""
+Training API
+"""
+
+
+@csrf_exempt
+def training_data(request):
+    """
+    Receives the training data labels, computes power consumption,
+    and stores them as Metadata
+    """
+
+    try:
+        if request.method == 'GET':
+            return HttpResponse(json.dumps(ERROR_INVALID_REQUEST), content_type="application/json")
+
+        if request.method == 'POST':
+            payload = json.loads(request.body)
+            print "\n[POST Request Received] -", sys._getframe().f_code.co_name
+            print payload
+
+            dev_id = payload['dev_id']
+            start_time = payload['start_time']
+            end_time = payload['end_time']
+            location = payload['location']
+            appliance = payload['appliance']
+
+            # Check if it is a registered user
+            user = user_exists(dev_id)
+            if not user:
+                return HttpResponse(json.dumps(TRAINING_UNSUCCESSFUL),
+                                    content_type="application/json")
+            else:
+                apt_no = user.apt_no
+                print "Apartment Number:", apt_no
+
+            # Compute Power
+            power = training_compute_power(apt_no, start_time, end_time)
+            print "Computed Power::", power
+
+            # See if entry exists for appliance-location combination
+            # Update power value if it exists
+            try:
+                r = Metadata.objects.get(apt_no__exact=apt_no, location__exact=location,
+                                         appliance__exact=appliance)
+                print "Metadata with entry:", r.apt_no, r.appliance, r.location, "exists"
+                # Update power
+                r.power = power
+                r.save()
+                print "Metadata record updated"
+            except Metadata.DoesNotExist, e:
+
+                # Store metadata
+                user = Metadata(
+                    apt_no=apt_no, appliance=appliance, location=location, power=power)
+                user.save()
+                print "Metadata creation successful"
+
+            payload = {}
+            payload['power'] = power
+            return HttpResponse(json.dumps(payload),
+                                content_type="application/json")
+
+    except Exception, e:
+
+            print "[TrainingDataException Occurred]::", e
+            return HttpResponse(json.dumps(TRAINING_UNSUCCESSFUL),
+                                content_type="application/json")
+
 
 """
 Upload API
@@ -107,6 +291,10 @@ def upload_data(request):
             filename = str(file_container.name)
             csvfile = file_container
             print "File received:", filename
+
+            # TEMPORARY CODE
+            users = determine_user_home_status(time.time() - 40, 1002)
+            print "Users at home for apt no. 1002", users
 
             # Store in the database
             if(import_from_file(filename, csvfile)):
@@ -281,180 +469,6 @@ def reassign_inference(request):
             print "[ReassignInferenceException Occurred]::", e
             return HttpResponse(json.dumps(REASSIGN_UNSUCCESSFUL),
                                 content_type="application/json")
-
-
-"""
-Training API
-"""
-
-
-@csrf_exempt
-def training_data(request):
-    """
-    Receives the training data labels, computes power consumption,
-    and stores them as Metadata
-    """
-
-    try:
-        if request.method == 'GET':
-            return HttpResponse(json.dumps(ERROR_INVALID_REQUEST), content_type="application/json")
-
-        if request.method == 'POST':
-            payload = json.loads(request.body)
-            print "\n[POST Request Received] -", sys._getframe().f_code.co_name
-            print payload
-
-            dev_id = payload['dev_id']
-            start_time = payload['start_time']
-            end_time = payload['end_time']
-            location = payload['location']
-            appliance = payload['appliance']
-
-            # Check if it is a registered user
-            user = user_exists(dev_id)
-            if not user:
-                return HttpResponse(json.dumps(TRAINING_UNSUCCESSFUL),
-                                    content_type="application/json")
-            else:
-                apt_no = user.apt_no
-                print "Apartment Number:", apt_no
-
-            # Compute Power
-            power = training_compute_power(apt_no, start_time, end_time)
-            print "Computed Power::", power
-
-            # See if entry exists for appliance-location combination
-            # Update power value if it exists
-            try:
-                r = Metadata.objects.get(apt_no__exact=apt_no, location__exact=location,
-                                         appliance__exact=appliance)
-                print "Metadata with entry:", r.apt_no, r.appliance, r.location, "exists"
-                # Update power
-                r.power = power
-                r.save()
-                print "Metadata record updated"
-            except Metadata.DoesNotExist, e:
-
-                # Store metadata
-                user = Metadata(
-                    apt_no=apt_no, appliance=appliance, location=location, power=power)
-                user.save()
-                print "Metadata creation successful"
-
-            payload = {}
-            payload['power'] = power
-            return HttpResponse(json.dumps(payload),
-                                content_type="application/json")
-
-    except Exception, e:
-
-            print "[TrainingDataException Occurred]::", e
-            return HttpResponse(json.dumps(TRAINING_UNSUCCESSFUL),
-                                content_type="application/json")
-"""
-Registration API
-"""
-
-
-@csrf_exempt
-def register_device(request):
-    """
-    Receives the registration requests from the mobile devices and
-    stores user-device and meter details in the database
-    """
-    print "Request received:", request.method
-
-    try:
-        if request.method == 'GET':
-            return HttpResponse(json.dumps(ERROR_INVALID_REQUEST), content_type="application/json")
-
-        if request.method == 'POST':
-            print "\n[POST Request Received]"
-
-            # print request.body
-            payload = json.loads(request.body)
-            print "POST Payload:\n", payload
-
-            reg_id = payload['registration_id']
-            user_name = payload['user_name']
-            email_id = payload['email_id']
-            dev_id = payload['dev_id']
-            apt_no = payload['apt_no']
-            home_ap = payload['home_ap']
-            other_ap = payload['other_ap']
-
-            print "\n--User Registration Details--"
-            print "RegID:", reg_id
-            print "Username:", user_name
-            print "Email ID:", email_id
-            print "Device ID:", dev_id
-            print "Apartment Number:", apt_no
-            print "Home AP:", home_ap
-            print "Other APs:", other_ap
-
-            if apt_no.isdigit():
-
-                # Store the meter details of the apt_no if the current user is the first
-                # member of the house that registers
-
-                apt_no = int(apt_no)
-                user_count = RegisteredUsers.objects.filter(apt_no__exact=apt_no).count()
-                print "Number of users registered for", apt_no, ":", user_count
-                if user_count == 0:
-                    # Get meter information for the apt_no for the apartment
-                    meters = get_meter_info(apt_no)
-
-                    # Store meter information in the DB
-                    for meter in meters:
-                        meter_uuid = meter['uuid']
-                        meter_type = meter['type']
-                        if meter_type == "Light Backup":
-                            meter_type = "Light"
-
-                        minfo_record = MeterInfo(
-                            meter_uuid=meter_uuid, meter_type=meter_type, apt_no=apt_no)
-                        minfo_record.save()
-
-                try:
-                    AccessPoints.objects.filter(apt_no__exact=apt_no).delete()
-
-                    # Store the access point details
-                    ap_record = AccessPoints(
-                        apt_no=apt_no, macid=home_ap['macid'], ssid=home_ap['ssid'], home_ap=True)
-                    ap_record.save()
-
-                    for ap in other_ap:
-                        ap_record = AccessPoints(
-                            apt_no=apt_no, macid=ap['macid'], ssid=ap['ssid'], home_ap=False)
-                        ap_record.save()
-                except Exception, e:
-                    print("[APSaveException]::%s" % (e))
-
-            try:
-                r = RegisteredUsers.objects.get(dev_id__exact=dev_id)
-                print "Registration with device ID", r.dev_id, "exists"
-                # Store user
-                r.reg_id = reg_id
-                r.name = user_name
-                r.is_active = True
-                r.modified_date = dt.datetime.fromtimestamp(time.time())
-                r.save()
-                print "Registration updated"
-            except RegisteredUsers.DoesNotExist, e:
-
-                # Store user
-                user = RegisteredUsers(
-                    dev_id=dev_id, reg_id=reg_id, apt_no=apt_no, name=user_name, email_id=email_id)
-                user.save()
-                print "Registration successful"
-            return HttpResponse(json.dumps(REGISTRATION_SUCCESS),
-                                content_type="application/json")
-
-    except Exception, e:
-
-        print "[DeviceRegistrationException Occurred]::", e
-        print "Registration unsuccessful"
-        return HttpResponse(json.dumps(REGISTRATION_UNSUCCESSFUL), content_type="application/json")
 
 
 @csrf_exempt
