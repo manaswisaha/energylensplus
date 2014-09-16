046a26857beaf981350966117cb575fc55a46e1a
Manaswi Saha <manaswis@iiitd.ac.in>
2014-09-16 09:00:42 +0530

    Changed function signature: determine user home status

--- energylenserver/wifi/functions.py
+++ energylenserver/wifi/functions.py
@@ -6,7 +6,10 @@ Contains helper functions
 def determine_user_home_status(event_time):
     """
     Determines if user is at home by seeing if the WiFi AP is visible
+
+    :param event_time:
+    :return list of users at home:
     """
     user_list = []
 
-    return True, user_list
+    return user_list
