1af1f6a809a06f5ab59eeec8aae865520c7a4922
Manaswi Saha <manaswis@iiitd.ac.in>
2014-09-17 16:36:22 +0530

    Removed print statements

--- energylenserver/core/functions.py
+++ energylenserver/core/functions.py
@@ -36,8 +36,8 @@ def determine_user_home_status(event_time, apt_no):
         data = mod_func.get_sensor_data("wifi", "test", start_time, end_time, dev_id_list)
         data_df = read_frame(data, verbose=False)
 
-        print "Users:", data_df.dev_id.unique()
-        print "Number of retrieved entries:", len(data_df)
+        # print "Users:", data_df.dev_id.unique()
+        # print "Number of retrieved entries:", len(data_df)
 
         # Check for each user, if he/she present in the home
         for idx in occupants_df.index:
@@ -49,7 +49,7 @@ def determine_user_home_status(event_time, apt_no):
                 continue
             # Check if any mac id matches with the home ap
             match_idx_list = np.where((df.macid == home_ap) & (df.rssi > WIFI_THRESHOLD))[0]
-            print("%s:%s" % (user_id, match_idx_list))
+            # print("%s:%s" % (user_id, match_idx_list))
             if len(match_idx_list) > 0:
                 user_list.append(user_id)
 
