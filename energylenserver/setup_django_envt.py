37a4f84d8b23c0797eb58d6a9d2639fa7e6af23d
Manaswi Saha <manaswis@iiitd.ac.in>
2014-09-18 11:33:55 +0530

    Made the setup django system agnostic

--- energylenserver/setup_django_envt.py
+++ energylenserver/setup_django_envt.py
@@ -4,15 +4,16 @@ Setup Django Environment
 import os
 import sys
 
-energyplus_path = '/home/manaswi/EnergyLensPlusCode/energylensplus/'
-energylenserver_path = '/home/manaswi/EnergyLensPlusCode/energylensplus/energylenserver/'
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+energyplus_path = BASE_DIR
+energylenserver_path = (energyplus_path + '/energylenserver')
 
 if energyplus_path not in sys.path:
-    sys.path.insert(0, energyplus_path)
+    sys.path.insert(1, energyplus_path)
     os.environ['DJANGO_SETTINGS_MODULE'] = "energylensplus.settings"
 
 if energylenserver_path not in sys.path:
-    sys.path.insert(1, energylenserver_path)
+    sys.path.insert(2, energylenserver_path)
 
 
 # print "SYSPATH", sys.path
