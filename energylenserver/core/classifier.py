013ae292936fa0076ccbf3488dec961c6f3ee1bf
Manaswi Saha <manaswis@iiitd.ac.in>
2014-09-18 21:55:19 +0530

    Added classifer and apportionment component

--- energylenserver/core/classifier.py
+++ energylenserver/core/classifier.py
@@ -9,29 +9,28 @@ Author: Manaswi Saha
 
 """
 
-import audio.classify_sound as cs
-import localize as lc
-import classify_accl as acl
+from energylenserver.core import audio as cs
+from energylenserver.core import location as lc
+from energylenserver.core import movement as acl
 
 
 stars = 40
 
 
-def classify_sound(train_csv, test_csv, apt_no, idx):
-
-    test_csv = cs.extract_features(test_csv, "test", apt_no, idx)
-    return cs.classify_sound(train_csv, test_csv, apt_no, idx)
-
-
 def classify_location(train_csv, test_csv, apt_no, idx):
-    train_csv = lc.format_data(train_csv, "train", apt_no, idx)
-    test_csv = lc.format_data(test_csv, "test", apt_no, idx)
+    print "--Classifying location--"
     return lc.classify_location(train_csv, test_csv, apt_no, idx)
 
 
 def classify_location_slice(train_csv, test_csv, apt_no, idx):
     test_csv = lc.format_data(test_csv, "test", apt_no, idx)
     return lc.classify_location_piecewise(train_csv, test_csv, apt_no, idx)
+
+
+def classify_sound(train_csv, test_csv, apt_no, idx):
+    print "--Classifying sound--"
+    test_csv = cs.extract_features(test_csv, "test", apt_no, idx)
+    return cs.classify_sound(train_csv, test_csv, apt_no, idx)
 
 
 def classify_accl(test_csv, apt_no, idx):
