272e455e456e922c24cb3c38dfb55ab44fce7257
Manaswi Saha <manaswis@iiitd.ac.in>
2014-10-30 09:59:40 +0530

    Created setup.py file with TODO

---
+++ setup.py
@@ -0,0 +1,6 @@
+# Setup script for EnergyLens Server
+
+# TODO
+# 1. Install the pre-requisite libraries - listed in requirements.txt
+# 2. Create a soft symbolic link - /usr/bin/energylens - to the shell management commands script
+# 3. Create a config file /etc/default/celeryd
