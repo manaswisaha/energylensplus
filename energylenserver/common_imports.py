6799bb3cdf0babae4161712492370994fe83352c
Manaswi Saha <manaswis@iiitd.ac.in>
2014-10-30 12:31:55 +0530

    Configured logger settings

--- energylenserver/common_imports.py
+++ energylenserver/common_imports.py
@@ -2,6 +2,6 @@ import logging
 from constants import *
 
 # Enable Logging
-logger = logging.getLogger('energy-lens')
+logger = logging.getLogger('energylensplus')
 # logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.INFO)
