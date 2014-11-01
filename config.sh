0397a5c703116009a58730c52a807a88d5796309
Manaswi Saha <manaswis@iiitd.ac.in>
2014-11-01 20:28:39 +0530

    Daemonized celery, created server config script

--- config.sh
+++ config.sh
@@ -1,26 +1,63 @@
-#Commands to run when deploying
+#!/bin/bash
+# Script to configure the system
 
+bash_file="$HOME/.bashrc"
 
-# Install server requirements 
-# Run only once TODO: Add a check
+# 1. Confirm the user that he is in the repo directory
+echo -e "Are you in EnergyLensPlus's installation directory (y/n)?"
+read isInstallDirectory
+if [ $isInstallDirectory = 'n' ] || [ $isInstallDirectory = 'N' ]; then
+	echo "Please go to the directory and run this file again"
+	exit 1
 
-# Apache
-sudo apt-get install apache2 apache2-prefork-dev
+# 2. If yes, then add an repo directory's (ELENSERVER) entry to ~/.bashrc file
+# and enable the changes
+elif [ $isInstallDirectory = 'y' ] || [ $isInstallDirectory = 'Y' ]; then
+	ELENSHOME=`pwd`
+	echo "Installation Directory: $ELENSHOME"
+	if [ -z `grep 'ELENSERVER=' $bash_file` ]; then
+		echo ``ELENSERVER=$ELENSHOME`` >> $bash_file
+		echo ``export ELENSERVER`` >> $bash_file
+	else
+		echo "Entry already made in the bashrc file"
+	fi
+	. $bash_file
+else
+	echo "Incorrect input. Please enter either y or n"
+	exit 1
+fi
 
-# mod_wsgi 
-sudo apt-get install mod_wsgi
+celery_config_file="${ELENSHOME}/config/celery-config"
 
-# MySQL Server
-sudo apt-get install mysql-server
+# 3. Copy ELENSERVER variable to the appropriate location in the celery-config file
+sed -i "s#\(CELERYD_CHDIR=\).*#\1$ELENSHOME/#g" $celery_config_file
+sed -i "s#\(CELERYBEAT_CHDIR=\).*#\1$ELENSHOME/#g" $celery_config_file
 
-# Install python libraries with Django web framework
-pip install -r requirements.txt
+sed -i "s#\(CELERYD_LOG_FILE=\).*#\1$ELENSHOME/logs/%N.log#g" $celery_config_file
+sed -i "s#\(CELERYD_PID_FILE=\).*#\1$ELENSHOME/logs/%N.pid#g" $celery_config_file
 
-#database_name = $1
+sed -i "s#\(CELERYBEAT_LOG_FILE=\).*#\1$ELENSHOME/logs/beat.log#g" $celery_config_file
+sed -i "s#\(CELERYBEAT_PID_FILE=\).*#\1$ELENSHOME/logs/beat.pid#g" $celery_config_file
 
-# Create database
-#mysql -u root -p -e "create database ".$database_name;
+# 4. Copy the three files from config directory to the appropriate locations
+	# celery-config --> /etc/default/celeryd
+	# celery-init-script --> /etc/init.d/celeryd
+	# celerybeat-init-script --> /etc/init.d/celerybeat
+sudo cp "${ELENSHOME}/config/celery-config" /etc/default/celeryd
+sudo cp "${ELENSHOME}/config/celery-init-script" /etc/init.d/celeryd
+sudo cp "${ELENSHOME}/config/celerybeat-init-script" /etc/init.d/celerybeat
 
-# Create tables
-#python manage.py sql energylenserver
-#python manage.py syncdb
+# 5. Give the init scripts execute privileges for all users
+sudo chmod 755 /etc/init.d/celeryd /etc/init.d/celeryd
+
+echo "Celery configured"
+
+# 6. Create the energylens command symbolic link
+if [ ! -f "/usr/bin/energylens" ];then
+	sudo ln -s "$ELENSHOME/energylenserver/management/energylens" /usr/bin/energylens
+	sudo chmod 755 /usr/bin/energylens
+	echo "'energylens' command created"
+fi
+
+echo "EnergyLensPlus system configuration complete! Use 'energylens' cmd to manage the server"
+echo "Usage: energylens {start|stop|status}"
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # Setup script for EnergyLens Server
 
 # TODO
-# 1. Install the pre-requisite libraries - listed in requirements.txt
-# 2. Create a soft symbolic link - /usr/bin/energylens - to the shell management commands script
-# 3. Create a config file /etc/default/celeryd
+# 1. Install the pre-requisite libraries - Run install.sh
+# 2. Run the config file
