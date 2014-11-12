be7e6f0b0222a23fa65e06d6ff07047afbcc0f3f
Manaswi Saha <manaswis@iiitd.ac.in>
2014-11-12 19:15:53 +0530

    Removed print statement

--- energylenserver/gcmxmppclient/msgclient.py
+++ energylenserver/gcmxmppclient/msgclient.py
@@ -90,7 +90,6 @@ class MessageClient:
         while True:
             try:
                 self.client.Process(1)
-                print("Waiting...")
                 # Restore connection if connection broken
                 if not self.client.isConnected():
                     logger.debug("Reconnecting..")
