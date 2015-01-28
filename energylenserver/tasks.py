8ad3255a7169e40e93dd9ca3d2f347167e837cd1
Manaswi Saha <manaswis@iiitd.ac.in>
2015-01-28 16:33:14 +0530

    Debugging

--- energylenserver/tasks.py
+++ energylenserver/tasks.py
@@ -156,6 +156,8 @@ def phoneDataHandler(filename, sensor_name, filepath, training_status, user):
                 # Don't go ahead if file already been dealt with
                 if filename in ftracker_df.filename.tolist():
                     flag = True
+                    logger.debug("File %s already inserted in the database!"
+                                 "flag:: %s", filename, flag)
 
                 # Create new file if entries older than 5 days
                 start_timestamp = ftracker_df.ix[0]['timestamp']
