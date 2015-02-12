5005af42ffc8eb2c87d3c9bd79eafc47086954dd
Manaswi Saha <manaswis@iiitd.ac.in>
2015-02-12 18:42:10 +0530

    Fixed bug

--- energylenserver/core/classifier.py
+++ energylenserver/core/classifier.py
@@ -336,7 +336,7 @@ def classify_location(apt_no, start_time, end_time, user, edge, n_users_at_home)
 
     except Exception, e:
         if str(e) == "(1205, 'Lock wait timeout exceeded; try restarting transaction')":
-            logger.exception("[ClassifyLocationException]:: %s", e)
+            logger.error("[ClassifyLocationException]:: %s", e)
             return location
         else:
             logger.exception("[ClassifyLocationException]:: %s", e)
